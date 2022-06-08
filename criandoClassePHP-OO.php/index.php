d3c30e77e899efc548d15a1e78edfe84edecdab9
MatheusBrandao525 <mafe123silva@gmail.com>
2022-06-08 14:23:31 -0400

    criando minha primeira classe

--- criandoClassePHP-OO.php/index.php
+++ criandoClassePHP-OO.php/index.php
@@ -4,21 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Minha primeira classe</title>
 </head>
 <body>
     <?php 
 
+        // incluido a classe no arquivo index...
         include_once 'Caneta.php';
 
+        // criando novo objeto para a classe...
         $obj = new Caneta;
+
+        // atribuindo valores ao atributos da classe...
         $obj->tipo = 'transparente';
         $obj->cor = 'azul';
         $obj->ponta = '0.5';
         $obj->tampada = false;
 
-        /* print_r($obj); */
-
+        // chamando método...
         $obj->escrever();
     ?>
 </body>
